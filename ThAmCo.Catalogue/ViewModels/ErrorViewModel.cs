2bfbe803f47a0c1d672668314016ac8df6ae0f9a
JLScott1999 <git@jlscott.co.uk>
2020-12-03 13:44:45 +0000

    Move error ViewModel to correct place

---
+++ ThAmCo.Catalogue/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace ThAmCo.Catalogue.ViewModels
+{
+    public class ErrorViewModel
+    {
+
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
+
+    }
+}
