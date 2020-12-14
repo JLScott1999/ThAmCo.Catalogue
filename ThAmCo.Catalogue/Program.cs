a6d37d251fbdfb349a16bca76c57976cbe23e568
JLScott1999 <git@jlscott.co.uk>
2020-12-14 10:50:34 +0000

    Update to .NET 5

--- ThAmCo.Catalogue/Program.cs
+++ ThAmCo.Catalogue/Program.cs
@@ -1,6 +1,7 @@
 namespace ThAmCo.Catalogue
 {
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
 
     public class Program
