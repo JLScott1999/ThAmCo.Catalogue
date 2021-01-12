d83f57084d082e59b17938e62681b08bf823dd5d
JLScott1999 <git@jlscott.co.uk>
2021-01-12 12:41:23 +0000

    Update product page review date format

--- ThAmCo.Catalogue/Models/ProductReviewModel.cs
+++ ThAmCo.Catalogue/Models/ProductReviewModel.cs
@@ -2,6 +2,7 @@ namespace ThAmCo.Catalogue.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -12,8 +13,10 @@ namespace ThAmCo.Catalogue.Models
 
         public string Name { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
         public DateTime Date { get; set; }
-
+        
         public string Description { get; set; }
 
     }
