755498b1162a301d9ff174bdd1d2524748621e46
JLScott1999 <git@jlscott.co.uk>
2021-01-12 09:38:58 +0000

    Update travis docker tags

--- .scripts/dotnet-publish.sh
+++ .scripts/dotnet-publish.sh
@@ -1,3 +1,4 @@
+
 case "$TRAVIS_BRANCH" in
   "main")
     DOCKER_TAG=latest
@@ -6,16 +7,18 @@ case "$TRAVIS_BRANCH" in
     DOCKER_TAG=dev
     ;;    
 esac
+VERSION_DOCKER_TAG=$(grep -A3 'appVersion:' /home/travis/build/JLScott1999/thamco.catalogue/chart/Chart.yaml | sed 's/^.*: //')
 
-DOCKER_TAG=$(grep -A3 'appVersion:' /home/travis/build/JLScott1999/thamco.catalogue/chart/Chart.yaml | sed 's/^.*: //')
-echo $DOCKER_TAG
-
-docker build -f ./ThAmCo.Catalogue/Dockerfile -t thamco.catalogue:$DOCKER_TAG ./ThAmCo.Catalogue --no-cache
+docker build -f ./ThAmCo.Catalogue/Dockerfile -t thamco.catalogue:$DOCKER_TAG -t thamco.catalogue:$VERSION_DOCKER_TAG ./ThAmCo.Catalogue --no-cache
 
 docker login docker.io -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker tag thamco.catalogue:$DOCKER_TAG docker.io/$DOCKER_USERNAME/thamco.catalogue:$DOCKER_TAG
 docker push docker.io/$DOCKER_USERNAME/thamco.catalogue:$DOCKER_TAG
+docker tag thamco.catalogue:$VERSION_DOCKER_TAG docker.io/$DOCKER_USERNAME/thamco.catalogue:$VERSION_DOCKER_TAG
+docker push docker.io/$DOCKER_USERNAME/thamco.catalogue:$VERSION_DOCKER_TAG
 
 docker login https://docker.pkg.github.com -u $GITHUB_USERNAME -p $GITHUB_PASSWORD
 docker tag thamco.catalogue:$DOCKER_TAG docker.pkg.github.com/$GITHUB_USERNAME/thamco.catalogue/thamco.catalogue:$DOCKER_TAG
 docker push docker.pkg.github.com/$GITHUB_USERNAME/thamco.catalogue/thamco.catalogue:$DOCKER_TAG
+docker tag thamco.catalogue:$VERSION_DOCKER_TAG docker.pkg.github.com/$GITHUB_USERNAME/thamco.catalogue/thamco.catalogue:$VERSION_DOCKER_TAG
+docker push docker.pkg.github.com/$GITHUB_USERNAME/thamco.catalogue/thamco.catalogue:$VERSION_DOCKER_TAG
